e855eefab76338259283609034fc7ae1b1f3d144
Frank Gonzalez <frankozoid@gmail.com>
2014-12-10 12:19:26 -0400

    Fibonacci
    Separated functions to give the nth Fib number and another to give a
    list of fibonacci numbers.

--- Fibs.hs
+++ Fibs.hs
@@ -6,11 +6,12 @@
 
 -}
 
+-- This function calculates the Fibonacci sequence.
+fib :: Int -> Int
+fib n
+    | n < 2 = n
+    | n >= 2 = fib(n-1) + fib(n-2)
+
 -- This function gives you a list of Fibonacci numbers.
 fibs :: [Int] -> [Int]
 fibs = map(fib)
-    where
-        -- This function calculates the Fibonacci sequence.
-        fib n
-            | n < 2 = n
-            | n >= 2 = fib(n-1) + fib(n-2)
