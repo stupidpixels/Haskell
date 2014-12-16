fe2274fb4dcd87710bed4aef4f22ac407550d0b3
Frank Gonzalez <frankozoid@gmail.com>
2014-12-16 03:35:27 -0400

    Pack
    Commented

--- Pack.hs
+++ Pack.hs
@@ -4,9 +4,10 @@
     PROBLEM 9 : Pack consecutive duplicates of list elements into sublists.
                 If a list contains repeated elements they should be placed
                 in separate sublists.
---}
+-}
 
--- Takes the first element of a list and looks for the others same as it.
+-- Looks at an element of a list and looks for the others same as it.
+-- Then goes to the next different element and recurses.
 pack (x:xs) = let (first,rest) = span (== x) xs
     in (x:first) : pack rest
 
