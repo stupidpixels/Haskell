aa90f2808f50fdb8f9b4951ba8fdc70e7658efaa
Frank Gonzalez <frankozoid@gmail.com>
2014-12-16 03:20:47 -0400

    Pack
    Takes a list and outputs a list with its consecutive elements separated.

--- Pack.hs
+++ Pack.hs
@@ -4,5 +4,10 @@
     PROBLEM 9 : Pack consecutive duplicates of list elements into sublists.
                 If a list contains repeated elements they should be placed
                 in separate sublists.
--}
+--}
 
+-- Takes the first element of a list and looks for the others same as it.
+pack (x:xs) = let (first,rest) = span (== x) xs
+    in (x:first) : pack rest
+
+pack [] = []
